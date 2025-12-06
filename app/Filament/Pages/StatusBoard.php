081743e135479945581f2de8f48ef21f5914cf55
combizera <ycombi349@gmail.com>
2025-12-06 19:02:12 -0300

    feat(StatusBoard): enhance board with edit actions and custom fields

--- app/Filament/Pages/StatusBoard.php
+++ app/Filament/Pages/StatusBoard.php
@@ -3,7 +3,12 @@
 namespace App\Filament\Pages;
 
 use App\Enums\OrderStatus;
+use App\Enums\RoastPoint;
 use App\Models\Order;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\EditAction;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Schema;
 use Illuminate\Database\Eloquent\Builder;
 use Relaticle\Flowforge\Board;
 use Relaticle\Flowforge\BoardPage;
@@ -22,11 +27,29 @@ class StatusBoard extends BoardPage
     public function board(Board $board): Board
     {
         return $board
-            ->query($this->getEloquentQuery())
-            //TODO: colocar um subtitle
+            ->query($this->getEloquentQuery()->with('customer.user'))
             ->recordTitleAttribute('customer.user.name')
             ->columnIdentifier('status')
             ->positionIdentifier('position')
+            ->cardAction('edit')
+            //TODO:mostrar pro gui se ele quer isso
+            ->cardActions([
+                EditAction::make()->model(Order::class),
+            ])
+            ->cardSchema(fn (Schema $schema) => $schema->components([
+                TextEntry::make('total_weight')
+                    ->label('Peso')
+                    ->badge()
+                    ->suffix(' g')
+                    ->icon('heroicon-s-scale'),
+
+                TextEntry::make('roast_point')
+                    ->label('Ponto')
+                    ->formatStateUsing(fn(RoastPoint $state) => $state->label())
+                    ->badge('info')
+                    ->icon('heroicon-s-fire'),
+
+            ]))
             ->columns([
                 Column::make(OrderStatus::PENDING->value)
                     ->label(OrderStatus::PENDING->label())

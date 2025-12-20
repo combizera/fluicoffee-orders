48b6359c2a1aef637aaebd3d61ed09d67f24fc81
combizera <ycombi349@gmail.com>
2025-12-20 17:06:13 -0300

    refactor(cta): rafael's suggestion

--- resources/views/components/cta.blade.php
+++ resources/views/components/cta.blade.php
@@ -1,14 +1,4 @@
-<section class="bg-coffee-700 rounded-2xl my-60 text-coffee-400 py-16 px-8 max-w-[600px] mx-auto relative">
-  <div class="absolute bottom-[120px] right-0">
-    <x-icons.gambiarra />
-  </div>
-  <div class="absolute bottom-0 right-[120px]">
-    <x-icons.gambiarra />
-  </div>
-  <div class="absolute bottom-[100px] right-[100px] z-2">
-    <x-icons.workaround />
-  </div>
-
+<section class="bg-coffee-700 rounded-2xl my-60 text-coffee-400 py-16 px-8 max-w-[600px] mx-auto relative group">
   <div class="flex flex-col items-start gap-4 mr-30">
     <h2 class="title text-5xl md:text-6xl lg:text-7xl leading-[1.2]">
       Pronto para <strong>começar?</strong>
@@ -19,8 +9,9 @@
     </p>
   </div>
 
-  <div class="absolute bottom-0 right-0 bg-coffee-400 w-[120px] h-[120px] flex items-end justify-end">
-    <a href="#" class="rounded-2xl flex items-center justify-center bg-coffee-700 w-[100px] h-[100px] block">
+  <div class="cta-button">
+    {{-- TODO: inserir link --}}
+    <a href="#">
       <x-icons.arrow />
     </a>
   </div>

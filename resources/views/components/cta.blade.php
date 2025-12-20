34fedf80684c7890c4e8ae1f347372ce21d0f082
combizera <ycombi349@gmail.com>
2025-12-19 21:31:01 -0300

    feat(cta)? create component

---
+++ resources/views/components/cta.blade.php
@@ -0,0 +1,27 @@
+<section class="bg-coffee-700 rounded-2xl my-60 text-coffee-400 py-16 px-8 max-w-[600px] mx-auto relative">
+  <div class="absolute bottom-[120px] right-0">
+    <x-icons.gambiarra />
+  </div>
+  <div class="absolute bottom-0 right-[120px]">
+    <x-icons.gambiarra />
+  </div>
+  <div class="absolute bottom-[100px] right-[100px] z-2">
+    <x-icons.workaround />
+  </div>
+
+  <div class="flex flex-col items-start gap-4 mr-30">
+    <h2 class="title text-5xl md:text-6xl lg:text-7xl leading-[1.2]">
+      Pronto para <strong>começar?</strong>
+    </h2>
+
+    <p class="font-bold text-lg text-[#d6c5be]">
+      Faça seu primeiro pedido e experimente a qualidade FluiCoffee.
+    </p>
+  </div>
+
+  <div class="absolute bottom-0 right-0 bg-coffee-400 w-[120px] h-[120px] flex items-end justify-end">
+    <a href="#" class="rounded-2xl flex items-center justify-center bg-coffee-700 w-[100px] h-[100px] block">
+      <x-icons.arrow />
+    </a>
+  </div>
+</section>
